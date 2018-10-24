721d3a4b1a503a180118547b1001e91db24e6fa5
Faris Habib <faris.habib.7@gmail.com>
2018-10-23 19:28:54 -0700

    Complete bare_minimum/promiseConstructor.js exercises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,56 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  /*
+    Initialize a new promise
+      pass in resolve and reject
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          reject(err)
+        } else {
+          var firstLine = split data
+          resolve(firstLine)
+        }
+      }).then((firstLine) => {}).catch((error) => {console.log(error)});
+  */
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        var firstLine = data.toString().split('\n')[0];
+        resolve(firstLine);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  /*
+    return new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) {
+          reject(err)
+        } else {
+          if (res.statusCode === 200) {
+            resolve(res.statusCode);
+          }
+        }
+      })
+    })
+  */
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (res.statusCode === 200) {
+          resolve(res.statusCode);
+        }
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
